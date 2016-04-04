8b4f8de98b6a66a97eeb4f54dc2cf7511335725c
unknown <henriq_182@hotmail.com>
2016-04-04 15:15:08 -0300

    keyupdown

--- js/starter.js
+++ js/starter.js
@@ -4,6 +4,7 @@
 
 //Gatilhos de eventos
 
+
 //Outras funcoes
 function LoadJSON(filename, callback){
 	var xmlhttp = new XMLHttpRequest();
@@ -15,5 +16,35 @@ function LoadJSON(filename, callback){
 	xmlhttp.open("GET", filename, true);
 	xmlhttp.send();
 };
+function doKeyDown(e) {
+
+alert( e.keyCode );
+switch (e.keyCode) {
+        case 37,38,39,40,32,80,27: console.log("vish " + e.keyCode); kDown(true,e.keyCode); break; //Left key
+        // case 38: alert("Up"); kDown(true); break; //Up key
+        // case 39: alert("Right"); kDown(true); break; //Right key
+        // case 40: alert("Down"); kDown(true); break; //Down key
+		// case 32: alert("Action"); kDown(true); break; //Down key
+		// case 80: alert("Pause"); kDown(true); break; //Down key
+		// case 27: alert("Esc"); kDown(true); break; //Down key
+        default: alert(e.keyCode); //Everything else
+    }
+
+}
+function doKeyUp(e) {
+
+alert( e.keyCode )
+switch (e.keyCode) {
+        case 37,38,39,40,32,80,27: console.log("parou " + e.keyCode); kDown(false,e.keyCode); break; //Left key
+        // case 38: alert("Up"); upDownDown=false; break; //Up key
+        // case 39: alert("Right"); rightDown=false; break; //Right key
+        // case 40: alert("Down"); downDown=false; break; //Down key
+		// case 32: alert("Action"); actionDown=false; break; //Down key
+		// case 80: alert("Pause"); pauseDown=false; break; //Down key
+		// case 27: alert("Esc"); escDown=false; break; //Down key
+        default: console.log("erroooou! "+ e.keyCode);icq(); //Everything else
+    }
+}
+
 
 //# sourceURL=starter.js
